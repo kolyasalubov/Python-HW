cb54ea33a4f2dd37e0123402cc383a167a44ea66
irynadmytrasevych <67744891+irynadmytrasevych@users.noreply.github.com>
2020-07-06 18:08:04 +0300

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,8 @@
+a=float(input("Enter number1: "))
+b=float(input("Enter number2: "))
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+print("a**b=",a**b)
+print("a//b=",a//b)
