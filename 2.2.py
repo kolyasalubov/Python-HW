54e2c3a2aab60ad111aa26b5b2e818591d2057b1
kolyasalubov <36229492+kolyasalubov@users.noreply.github.com>
2020-07-28 23:44:33 +0300

    Update 2.2.py

--- 2.2.py
+++ 2.2.py
@@ -1,5 +1,7 @@
-num=str(input("Enter number: "))
+num=str(input("Enter number: "))   
 result=int(num[0])*int(num[1])*int(num[2])*int(num[3])
 print("Result: ", result)
 print("Reversed number: ", list(reversed(num)))
 print("Sorted: ", sorted(num))
+
+в першій стрічці str не потрібно, input завжди повертає стрінгу
